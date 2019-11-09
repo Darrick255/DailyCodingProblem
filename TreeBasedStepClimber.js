5408a83288589bd7135175e2123d087f4beed102
Darrick Fletcher <Darrick255@users.noreply.github.com>
2019-11-08 16:39:14 -0800

    Update TreeBasedStepClimber.js
    add some extra broken methods. to compare against

--- TreeBasedStepClimber.js
+++ TreeBasedStepClimber.js
@@ -9,7 +9,7 @@ function staircase(N,jumps){
       var node = BuildNode(cnode,jumps[j])
       if (node.sum == N){
         uniques.push(node);
-        console.log('found unique, curently: ' +uniques.length);
+        //console.log('found unique, curently: ' +uniques.length);
       } else if (node.sum < N){
         stack.push(node);
       } else if(node.sum >N){
@@ -21,12 +21,10 @@ function staircase(N,jumps){
   console.log(uniques.length);
   if (uniques.length<=500){
     for(var i=0; i<uniques.length;i++){
-        console.log(uniques[i].jumps.toString())
+        //console.log(uniques[i].jumps.toString())
     }
   }
-  return uniques;
-  
-  
+  return uniques.length;
   
 }
 
@@ -38,4 +36,72 @@ function BuildNode(parent, mod){
     node.jumps.push(mod)
   }
   return node
+}
+
+function num_ways(n){
+    if (n==0 || n==1){
+        return 1;
+    }
+    else {
+        return num_ways(n-1)+ num_ways(n-2)
+    }
+}
+
+function num_ways_bottom_up(n){
+    if(n==0||n==1){
+        return 1;
+    }
+    var nums =[]
+    nums[0] ==1;
+    nums[1] ==1;
+    for(var i=2;i<=n;i++){
+        nums[i] = nums[i-1] + nums[i-2]
+    }
+    return nums[n]
+}
+
+
+function num_ways_x(n,s){
+    if (n==0){
+        return 1;
+    }
+    var total = 0;
+    for(var i=0;i<s.length;i++){
+        var el = s[i];
+        if(n-el >=0){
+            total+= num_ways_x(n-el,s)
+        }
+    }
+    return total;
+}
+
+function num_ways_bottom_up_x(n,s){
+    if (n==0){
+        return 1;
+    }
+    debugger
+    var nums =[]
+    nums[0] ==1;
+    for(var i=1;i<=n;i++){
+        var total = 0;
+        for(var j=0;j<s.length;j++){
+            var el = s[j];
+            if (i-el >=0){
+                total += nums[i-el]
+            }
+        }
+        nums[i]=total;
+    }
+    return nums[n]
+}
+
+function testing(min,max,steps){
+    for (var t=min;t<=max;t++){
+        console.log('t value is ' + t + ', steps value is: ' + JSON.stringify(steps));
+        console.log(staircase(t,steps));
+        console.log(num_ways_bottom_up_x(t,steps));
+        if(num_ways_bottom_up_x(t,steps)==staircase(t,steps)){
+            console.log('true');
+        }
+    }
 }
